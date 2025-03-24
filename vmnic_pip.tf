7da5e93bdbb4887c383d4ee51467d0928b644cb1
tarunmalladi <malladitarun100@gmail.com>
2025-03-24 12:38:50 +0530

    nic modified

--- vmnic_pip.tf
+++ vmnic_pip.tf
@@ -3,7 +3,7 @@ resource "azurerm_public_ip" "vm_public_ip" {
   name                = "vm-public-ip-${count.index}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "nic" {
@@ -15,7 +15,7 @@ resource "azurerm_network_interface" "nic" {
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = "Static"
     public_ip_address_id          = azurerm_public_ip.vm_public_ip[count.index].id  # This is the line that needs to be changed
   }
 }
