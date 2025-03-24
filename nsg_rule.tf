026c5f8d86fd81e7c452f6201b4fc739bf975dfc
tarunmalladi <malladitarun100@gmail.com>
2025-03-24 15:02:23 +0530

    nic ssh

--- nsg_rule.tf
+++ nsg_rule.tf
@@ -14,4 +14,15 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+    security_rule {
+    name                       = "allow_ssh"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
