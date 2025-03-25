58c2ba8208ef55d4dd73ed37dd8685c09fcce3da
tarunmalladi <malladitarun100@gmail.com>
2025-03-25 12:51:09 +0530

    code added

--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
@@ -7,6 +5,8 @@ terraform {
       version = "=4.23.0"
     }
   }
+
+  backend "azurerm" {}
 }
 
 # Configure the Microsoft Azure Provider
@@ -15,4 +15,5 @@ provider "azurerm" {
   subscription_id = "07f136e5-d168-4950-a26f-e1a528a1a87c"
   client_id = "a37a1b53-6024-4931-8c80-b5aaa26154e7"
   tenant_id = "77711e6a-2454-45d2-9683-aa6b2f67926f"
+
 }
