d0c673ec39ecd86094de091f6f43edcb32c0a644
tarunmalladi <malladitarun100@gmail.com>
2025-03-25 13:01:27 +0530

    code added

--- provider.tf
+++ provider.tf
@@ -6,7 +6,12 @@ terraform {
     }
   }
 
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = var.key
+  }
 }
 
 # Configure the Microsoft Azure Provider
