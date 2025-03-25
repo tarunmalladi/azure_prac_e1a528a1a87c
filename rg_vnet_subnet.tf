34b465e5e9ee26aa01c53a33f4e6c68d464372f8
tarunmalladi <malladitarun100@gmail.com>
2025-03-25 12:10:43 +0530

    code added

--- rg_vnet_subnet.tf
+++ rg_vnet_subnet.tf
@@ -15,4 +15,5 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+  
 }
